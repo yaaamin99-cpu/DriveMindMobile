d89e671df0b857d4ef2f3d893d318fc2276bbef0
Bünyamin <yaaamin.99@gmail.com>
2025-09-18 05:18:53 +0200

    fix(android): add RN deps and simplify MainActivity/MainApplication to stable template

--- android/app/src/main/java/com/drivemindmobile/MainApplication.kt
+++ android/app/src/main/java/com/drivemindmobile/MainApplication.kt
@@ -1,43 +1,24 @@
 package com.drivemindmobile
 
 import android.app.Application
-import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
-import com.facebook.react.ReactHost
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
-import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
-import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
-import com.facebook.react.defaults.DefaultReactNativeHost
+import com.facebook.react.PackageList
 import com.facebook.soloader.SoLoader
 
 class MainApplication : Application(), ReactApplication {
 
-  override val reactNativeHost: ReactNativeHost =
-      object : DefaultReactNativeHost(this) {
-        override fun getPackages(): List<ReactPackage> =
-            PackageList(this).packages.apply {
-              // Packages that cannot be autolinked yet can be added manually here, for example:
-              // add(MyReactNativePackage())
-            }
-
-        override fun getJSMainModuleName(): String = "index"
-
+    private val mReactNativeHost: ReactNativeHost = object : ReactNativeHost(this) {
         override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
-
-        override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
-        override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
-      }
-
-  override val reactHost: ReactHost
-    get() = getDefaultReactHost(applicationContext, reactNativeHost)
-
-  override fun onCreate() {
-    super.onCreate()
-    SoLoader.init(this, false)
-    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-      // If you opted-in for the New Architecture, we load the native entry point for this app.
-      load()
+        override fun getPackages(): MutableList<ReactPackage> = PackageList(this).packages
+        override fun getJSMainModuleName(): String = "index"
     }
-  }
+
+    override fun getReactNativeHost(): ReactNativeHost = mReactNativeHost
+
+    override fun onCreate() {
+        super.onCreate()
+        SoLoader.init(this, false)
+    }
 }
